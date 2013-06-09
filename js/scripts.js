587aa01923fc0fa17cd389548ab7f4974a597e52
Rich Buff <rich.buff@gmail.com>
2013-06-09 16:10:43 -0500

    update

--- js/scripts.js
+++ js/scripts.js
@@ -4,22 +4,22 @@
 			autoplay_slideshow: true
 		});
 		
-		$('.menu ul li').hover(
+   		$('.menu ul li').hover(
 		function () {
 			$('.sub_menu', this).stop(true, true).slideDown(); /*slideDown the subitems on mouseover*/
 		}, function () {
-			$('.sub_menu', this).stop(true, true).slideUp();  /*slideUp the subitems on mouseout*/
+		    $('.sub_menu', this).stop(true, true).slideUp();  /*slideUp the subitems on mouseout*/
 		});
 
         handleWidth();
     }
 
 	function showSarah() {
-		var api_gallery=['media/img_9530_med.jpeg','media/meandsarah.jpeg','media/oysterrace1.png','media/sarah1-2_med.png'];
+	    var api_gallery = ['images/sarah/tough-enough.jpeg', 'images/sarah/meandsarah.jpeg', 'images/sarah/oysterrace1.png', 'images/sarah/upside-down.jpeg'];
 		var api_titles=['Introducing Sarah Bird','Sarah and Jill','At the Oyster Race','image 4'];
 		var api_descriptions=['Description 1','Description 2','Description 3','Desc 4'];
 		
-		$.prettyPhoto.open(api_gallery,api_titles,api_descriptions);
+		$.prettyPhoto.open(api_gallery, api_titles, api_descriptions);
 		return false;
 	}
 
