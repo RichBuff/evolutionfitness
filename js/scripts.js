b1cab8acab8ca714fb0279bf59813247ee34fc3c
Rich Buff <rich.buff@gmail.com>
2013-06-09 12:53:21 -0500

    Initial

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+
+    function initPage(args) {
+   		$("a[rel^='prettyPhoto']").prettyPhoto({
+			autoplay_slideshow: true
+		});
+		
+		$('.menu ul li').hover(
+		function () {
+			$('.sub_menu', this).stop(true, true).slideDown(); /*slideDown the subitems on mouseover*/
+		}, function () {
+			$('.sub_menu', this).stop(true, true).slideUp();  /*slideUp the subitems on mouseout*/
+		});
+
+        handleWidth();
+    }
+
+	function showSarah() {
+		var api_gallery=['','media/img_9530_med.jpeg','media/meandsarah.jpeg','media/oysterrace1.png','media/sarah1-2_med.png'];
+		var api_titles=['Sarah\'s really cool description','Introducing Sarah Bird','Sarah and Jill','At the Oyster Race','image 4'];
+		var api_descriptions=['Hello, world','Description 1','Description 2','Description 3','Desc 4'];
+		
+		$.prettyPhoto.open(api_gallery,api_titles,api_descriptions);
+		return false;
+	}
+
+function handleWidth() {
+    var wi = $($("#menu")).width();
+    if (wi < 530) {
+        $("#trainers").html("The Trainers");
+        $("#menu ul li a").addClass("smaller-font");
+    }
+    else {
+        $("#trainers").html("Meet The Trainers");
+        $("#menu ul li a").removeClass("smaller-font");        
+    }
+}
